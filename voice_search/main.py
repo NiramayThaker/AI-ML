8cab0da44d0a3afe94bf5a6a84b4b9d9c2c95fe8
Niramay Thaker <70151582+NiramayThaker@users.noreply.github.com>
2022-08-16 18:51:48 +0530

    Main file to start AI voice search

---
+++ voice_search/main.py
@@ -0,0 +1,16 @@
+from browsing_AI import *
+
+if __name__ == "__main__":
+	speak("at your service sir")
+	while True:
+		wake_up_AI = take_command()
+		if "search" in wake_up_AI:
+			speak("what to search sir ?")
+			live_info()
+
+		if "good bye" in wake_up_AI:
+			speak("Good bye sir")
+			exit()
+
+		if "go to sleep" in wake_up_AI:
+			wait()
