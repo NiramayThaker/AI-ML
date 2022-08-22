e7cf85177f754ca944b5136e37c8f86cdcce652f
Niramay Thaker <70151582+NiramayThaker@users.noreply.github.com>
2022-08-22 18:18:44 +0530

    Update simple_chat_bot.py

--- chat_bot/simple_chat_bot.py
+++ chat_bot/simple_chat_bot.py
@@ -1,3 +1,77 @@
+# import numpy
+# import tflearn
+# import tensorflow
+# import random
+# import json
+# import nltk
+# from nltk.stem.lancaster import LancasterStemmer
+#
+# stemmer = LancasterStemmer()
+#
+# # Phase 1: Making list to save data and accessing data from json file
+# with open("intents.json") as file:
+# 	data = json.load(file)
+#
+# words = []
+# labels = []
+# docs_x = []
+# docs_y = []
+#
+# for intent in data["intents"]:
+# 	for pattern in intent["patterns"]:
+# 		wrds = nltk.word_tokenize(pattern)
+# 		words.extend(wrds)
+# 		docs_x.append(pattern)
+# 		docs_y.append(intent["tag"])
+#
+# 	if intent["tag"] not in labels:
+# 		labels.append(intent["tag"])
+#
+#
+# # Phase 2: Will give data to fit in bag of words bag
+#
+# words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+# words = sorted(list(set(words)))
+# labels = sorted(labels)
+#
+# training = []
+# output = []
+#
+# out_empty = [0 for _ in range(len(labels))]
+#
+# for x, doc in enumerate(docs_x):
+# 	bag = []
+# 	wrds = [stemmer.stem(w) for w in doc]
+#
+# 	for w in words:
+# 		if w in wrds:
+# 			bag.append(1)
+# 		else:
+# 			bag.append(0)
+#
+# 	output_row = out_empty[:]
+# 	output_row[labels.index(docs_y[x])] = 1
+#
+# 	training.append(bag)
+# 	output.append(output_row)
+#
+# training = numpy.array(training)
+# output = numpy.array(output)
+#
+#
+# # Phase 3: Complete AI and neural network for chatbot
+# # tensorflow.reset_default_graph()
+#
+# # Neural network layer
+# net = tflearn.input_data(shape=[None, len(training[0])])
+# net = tflearn.fully_connected(net, 8)
+# net = tflearn.fully_connected(net, 8)
+# net = tflearn.fully_connected(net, len(output), activation="softmax")
+# net = tflearn.regression(net)
+#
+# model = tflearn.DNN(net)
+# model.fit(training, output, batch_size=8, show_metric=True)
+# model.save("model.tflearn")
 
 
 import nltk
@@ -11,6 +85,7 @@ import pickle
 
 stemmer = LancasterStemmer()
 
+# Phase 1: Making list to save data and accessing data from json file
 with open("intents.json") as file:
 	data = json.load(file)
 
@@ -18,6 +93,7 @@ try:
 	with open("data.pickle", "rb") as f:
 		words, labels, training, output = pickle.load(f)
 except:
+	# Phase 2: Will give data to fit in bag of words bag
 	words = []
 	labels = []
 	docs_x = []
@@ -66,6 +142,7 @@ except:
 	with open("data.pickle", "wb") as f:
 		pickle.dump((words, labels, training, output), f)
 
+# Phase 3: Complete AI and neural network for chatbot
 # tensorflow.reset_default_graph()
 tensorflow.compat.v1.reset_default_graph()
 
@@ -77,6 +154,7 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
+# Phase 4: Saving model for reuse
 try:
 	model.load("model.tflearn")
 except:
@@ -84,6 +162,7 @@ except:
 	model.save("model.tflearn")
 
 
+# Phase 5: Utility function
 def bag_of_words(s, words):
 	"""
 	will generate bag of words (binary[1, 0])
