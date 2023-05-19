16e2da4e5c2aced2697cf5f86cb33f37bd862011
Niramay Thaker <70151582+NiramayThaker@users.noreply.github.com>
2023-05-19 20:33:21 +0530

    Basic Image reading and manipulation

--- computer_vision/image_processing/main.py
+++ computer_vision/image_processing/main.py
@@ -17,44 +17,41 @@ img_mpl = plt.imread(cat_files[20])
 img_cv2 = cv2.imread(cat_files[20])
 
 # Plotting the image pixels
-# pd.Series(img_mpl.flatten()).plot(kind='hist',
-#                                   bins=50,
-#                                   title='Distribution of Pixel Values')
-# plt.show()
-
+pd.Series(img_mpl.flatten()).plot(kind='hist',
+								  bins=50,
+								  title='Distribution of Pixel Values')
+plt.show()
 
 # Displaying image
-# fig, ax = plt.subplots(figsize=(10, 10))
-# ax.imshow(img_mpl)
-# ax.axis('off')
-# plt.show()
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.imshow(img_mpl)
+ax.axis('off')
+plt.show()
 
 # Display RGB Channels of our image
-# fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-# axs[0].imshow(img_mpl[:, :, 0], cmap='Reds')
-# axs[1].imshow(img_mpl[:, :, 1], cmap='Greens')
-# axs[2].imshow(img_mpl[:, :, 2], cmap='Blues')
-# axs[0].axis('off')
-# axs[1].axis('off')
-# axs[2].axis('off')
-# axs[0].set_title('Red channel')
-# axs[1].set_title('Green channel')
-# axs[2].set_title('Blue channel')
-# plt.show()
-
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+axs[0].imshow(img_mpl[:, :, 0], cmap='Reds')
+axs[1].imshow(img_mpl[:, :, 1], cmap='Greens')
+axs[2].imshow(img_mpl[:, :, 2], cmap='Blues')
+axs[0].axis('off')
+axs[1].axis('off')
+axs[2].axis('off')
+axs[0].set_title('Red channel')
+axs[1].set_title('Green channel')
+axs[2].set_title('Blue channel')
+plt.show()
 
 # Matplotlib vs cv2 Numpy Arrays
 # cv2 reads in channels as BGR
 # matplotlib reads in channels as RGB
-# fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-# axs[0].imshow(img_cv2)
-# axs[1].imshow(img_mpl)
-# axs[0].axis('off')
-# axs[1].axis('off')
-# axs[0].set_title('CV2 Image')
-# axs[1].set_title('Matplotlib Image')
-# plt.show()
-
+fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+axs[0].imshow(img_cv2)
+axs[1].imshow(img_mpl)
+axs[0].axis('off')
+axs[1].axis('off')
+axs[0].set_title('CV2 Image')
+axs[1].set_title('Matplotlib Image')
+plt.show()
 
 # Image Manipulation
 img = plt.imread(dog_files[11])
@@ -71,7 +68,6 @@ ax.axis('off')
 ax.set_title('Grey Image')
 plt.show()
 
-
 # Different Size
 img_resize = cv2.resize(img, (100, 200))
 fig, ax = plt.subplots(figsize=(8, 8))
@@ -79,8 +75,34 @@ ax.imshow(img_resize)
 ax.axis('off')
 plt.show()
 
-img_resize = cv2.resize(img, (5000, 5000), interpolation = cv2.INTER_CUBIC)
+img_resize = cv2.resize(img, (5000, 5000), interpolation=cv2.INTER_CUBIC)
 fig, ax = plt.subplots(figsize=(8, 8))
 ax.imshow(img_resize)
 ax.axis('off')
 plt.show()
+
+# Sharpen Image
+kernel_sharpening = np.array([[-1, -1, -1],
+							  [-1, 9, -1],
+							  [-1, -1, -1]])
+
+sharpened = cv2.filter2D(img, -1, kernel_sharpening)
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(sharpened)
+ax.axis('off')
+ax.set_title('Sharpened Image')
+plt.show()
+
+# Blurring the image
+kernel_3x3 = np.ones((3, 3), np.float32) / 9
+blurred = cv2.filter2D(img, -1, kernel_3x3)
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(blurred)
+ax.axis('off')
+ax.set_title('Blurred Image')
+plt.show()
+
+# Save Image
+plt.imsave('mpl_dog.png', blurred)
+cv2.imwrite('cv2_dog.png', blurred)
