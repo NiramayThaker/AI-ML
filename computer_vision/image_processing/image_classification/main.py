701318c35c338ec171a749d6402ab18321112bdb
Niramay Thaker <70151582+NiramayThaker@users.noreply.github.com>
2023-05-30 17:16:06 +0530

    Update main.py

--- computer_vision/image_processing/image_classification/main.py
+++ computer_vision/image_processing/image_classification/main.py
@@ -1,7 +1,10 @@
+import random
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
+# Loading DataSets
 X_train = np.loadtxt('input.csv', delimiter=",")
 Y_train = np.loadtxt('labels.csv', delimiter=",")
 
@@ -14,3 +17,46 @@ Y_train = Y_train.reshape(len(Y_train), 1)
 X_test = X_test.reshape(len(X_test), 100, 100, 3)
 Y_test = Y_test.reshape(len(Y_test), 1)
 
+X_train = X_train/255.0
+Y_train = Y_train/255.0
+
+# print(X_train[1:])
+
+# idx = random.randint(0, len(X_train))
+# plt.imshow(X_train[idx, :])
+# plt.show()
+
+# MODEL Creation
+model = Sequential ([
+    Conv2D(32, (3,3), activation =  'relu', input_shape = (100, 100, 3)),
+    MaxPooling2D((2,2)),
+
+    Conv2D(32, (3,3), activation = 'relu'),
+    MaxPooling2D((2,2)), 
+
+    Flatten(),
+    Dense(64, activation = 'relu'),
+    Dense(1, activation = 'sigmoid')
+])
+
+# model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+# model.fit(X_train, Y_train, epochs = 5, batch_size = 64)
+
+# model.evaluate(X_test, Y_test)
+
+# Making predictions
+idx2 = random.randint(0, len(Y_test))
+plt.imshow(Y_test[idx2, :])
+plt.show()
+
+
+y_pred = model.predict(X_test[idx2, :].reshape(1, 100, 100, 3))
+y_pred = y_pred > 0.5
+
+if(y_pred == 0):
+    pred = 'dog'
+else:
+    pred = 'cat'
+    
+print("Our model says it is a :", pred)
